e9208a97b071cf90e9223a7d9e771c01c1bc9808
Emil VATAI <emil.vatai@gmail.com>
2025-11-18 19:24:58 +0900

    Build from scratch

--- cpwe/foo.py
+++ cpwe/foo.py
@@ -3,14 +3,13 @@
 import cython
 from cython.cimports import isl
 
-# pet_scop_p_vec = cython.typedef(vector[ctadashi.pet_scop_p])
-
 p_ctx = cython.typedef(cython.pointer[isl.isl_ctx])
 p_union_map = cython.typedef(cython.pointer[isl.isl_union_map])
 ctx = cython.declare(p_ctx)
 ctx = isl.isl_ctx_alloc()
 umap = cython.declare(p_union_map)
 umap = isl.isl_union_map_read_from_str(ctx, "[N] -> {}")
+
 print(isl.isl_union_map_to_str(umap))
 print("{ctx=}")
 isl.isl_ctx_free(ctx)
--- third_party/install.sh
+++ third_party/install.sh
@@ -2,15 +2,18 @@
 set -x
 set -e
 
-# git init /project/isl
-# git -C /project/isl remote add origin /project/third_party/isl.bundle
-# git -C /project/isl fetch
+git init /project/isl
+git -C /project/isl remote add origin /project/third_party/isl.bundle
+git -C /project/isl fetch
+git -C /project/isl checkout master
 
-# # yum install -y isl-devel
-# yum install -y gmp-c++ gmp-devel
-# cd /project/isl/
-# ./autogen.sh
-# ./configure --disable-shared
-# make -j
+# yum install -y isl-devel
+yum install -y gmp-c++ gmp-devel
+cd /project/isl/
+./autogen.sh
+./configure
+make -j
+echo "=== include ==="
+ls /project/isl/include
 
-cp -r /isl /project
+# cp -r /isl /project
