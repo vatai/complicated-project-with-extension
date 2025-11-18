5dd3c31bdf0d74f5b3d7b36496809f13bfdad5b4
Emil VATAI <emil.vatai@gmail.com>
2025-11-18 12:38:20 +0900

    Move everything to cpwe

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-# ext-modules = [ {name = "foo", sources = ["ext/foo.c"]} ]
-
 from Cython.Build import cythonize
 from setuptools import Extension, setup
 
 ext_modules = [
     Extension(
-        "foo",
-        sources=["ext/foo.c"],
+        "cpwe.foo",
+        sources=[
+            "cpwe/foo.c",
+        ],
         # libraries=["pet", "isl"],
         # library_dirs=["/home/vatai/code/tadashi/ctadashi/lib"],
         # runtime_library_dirs=["/home/vatai/code/tadashi/ctadashi/lib"],
--- tests/test_local_uv_project_from_scratch.sh
+++ tests/test_local_uv_project_from_scratch.sh
@@ -4,14 +4,15 @@ set -e
 cd "$(git rev-parse --show-toplevel)"
 rm -rf wheelhouse
 uvx cibuildwheel
-WHEEL="$(pwd)/wheelhouse/complicated_project_with_extension-42-cp313-cp313-manylinux1_x86_64.manylinux_2_28_x86_64.manylinux_2_5_x86_64.whl"
+WHEEL="$(pwd)/wheelhouse/cpwe-42-cp313-cp313-manylinux1_x86_64.manylinux_2_28_x86_64.manylinux_2_5_x86_64.whl"
 rm -rf /tmp/test_site
 mkdir -p /tmp/test_site
 cd /tmp/test_site
 uv init
 uv add "$WHEEL"
 cat > run.py << EOF
-import complicated_project_with_extension as cpwe
+import cpwe
+import foo
 print(f"{cpwe.__file__=}")
 EOF
 
